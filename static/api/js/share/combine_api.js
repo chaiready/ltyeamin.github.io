8ed090a4bbd23597df824a72dc2d2f70cf0a1502
tong.li <lt_alex@163.com>
2019-06-08 20:20:58 +0800

    Site updated: 2019-06-08 20:20:57

---
+++ static/api/js/share/combine_api.js
@@ -0,0 +1,1 @@
+window._bd_share_main.F.module("share/combine_api",function(n,a,i){var e=(n("base/tangram").T,n("base/class").Class),s=n("share/api_base");a.CombineApi=e.create(function(n){var a=this,i=null,e=null;a.setApi=function(n,a){i=n,e=a},a._init=function(){i&&e&&i.on("sharecompleted",function(n){})}},s.ApiBase)});
